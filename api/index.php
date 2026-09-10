49196bc5a767f41ca2e39fd71347eaa376551690
Innocent Steven Mwalimu <innocentmwalimu256@gmail.com>
2026-09-11 00:30:42 +0300

    Set SESSION_DRIVER to file in /tmp/storage

--- api/index.php
+++ api/index.php
@@ -59,8 +59,8 @@ try {
     $_ENV['APP_MAINTENANCE_DRIVER'] = 'file';
     $_SERVER['APP_MAINTENANCE_DRIVER'] = 'file';
 
-    putenv("SESSION_DRIVER=cookie");
-    $_ENV['SESSION_DRIVER'] = 'cookie';
+    putenv("SESSION_DRIVER=file");
+    $_ENV['SESSION_DRIVER'] = 'file';
 
     putenv("CACHE_STORE=array");
     $_ENV['CACHE_STORE'] = 'array';
